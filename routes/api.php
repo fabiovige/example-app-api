e961473c3363d2317e246c14ffc6c6ca4b7ff48c
Fabio Vige <fabiovige@gmail.com>
2025-02-22 06:56:02 -0300

    rotas iniciais API e WEB

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function () {
+    return 'API';
 });
+
+/*Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});*/
